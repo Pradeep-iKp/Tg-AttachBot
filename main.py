fc1c1e4e9f1aa5be9bab5b09be28f16e95a07ff0
P̤radeep <78857587+Pradeep-iKp@users.noreply.github.com>
2021-11-15 17:19:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ Bot = Client(
     api_hash = os.environ["API_HASH"],
 )
 
-BOT_TEXT = """Tg-AttachBot/blob/main/START.jpg"""
+BOT_TEXT = """"""
 RATE_TEXT = """**⭐ --Rate Our Service-- ⭐**
 If you like our service please rate it! We are waiting for your feedback. 🤗
 """
@@ -63,8 +63,8 @@ async def send_msg(user_id, message):
 async def start(bot, update):
     if not await db.is_user_exist(update.from_user.id):
             await db.add_user(update.from_user.id)
-    await update.reply_text(
-	text=BOT_TEXT,
+    await update.reply_sticker(
+        sticker=BOT_TEXT
         disable_web_page_preview=True,
     )
 
